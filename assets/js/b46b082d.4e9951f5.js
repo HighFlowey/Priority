7f5750ee631c2efad4bf9df3821146cc8f90a5de
HighFlowey <HighFlowey@gmail.com>
2023-12-25 21:26:09 +0330

    Built and published by Moonwave

--- assets/js/b46b082d.4e9951f5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[464],{59389:e=>{e.exports=JSON.parse('{"functions":[],"properties":[],"types":[],"name":"Memory","desc":"[Priority] classes get stored here so [Constructor.new] doesn\'t create multiple classes for one [Humanoid].","private":true,"source":{"line":17,"path":"src/init.lua"}}')}}]);
